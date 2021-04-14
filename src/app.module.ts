f830dcc32c958d2ae0bae2c60f38ea9d99123b8c
jasonlimantoro <jason@ewburgers.com>
2021-04-15 01:41:10 +0800

    wip: initial setup for nestjs->graphql->type-graphql->prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { TypeGraphQLModule } from 'typegraphql-nestjs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostModule } from './post/post.module';
+
+const prisma = new PrismaClient();
 
 @Module({
-  imports: [],
+  imports: [
+    TypeGraphQLModule.forRoot({
+      emitSchemaFile: true,
+      validate: false,
+      dateScalarMode: 'timestamp',
+      context: ({ req }) => ({ currentUser: req.user, prisma }),
+    }),
+    PostModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
