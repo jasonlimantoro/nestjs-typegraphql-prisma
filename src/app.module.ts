6e10586060808dc0040c6b0e2afecd218c5b83b6
jasonlimantoro <jason@ewburgers.com>
2021-04-15 23:17:31 +0800

    refactor: use centralized prisma client object

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { prismaClient as prisma } from '../prisma/client';
 import { TypeGraphQLModule } from 'typegraphql-nestjs';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
-
-const prisma = new PrismaClient();
 
 @Module({
   imports: [
