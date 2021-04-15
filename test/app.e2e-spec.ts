7fb5a270858f85f2d8508461760c7876b51a1e0e
jasonlimantoro <jason@ewburgers.com>
2021-04-16 03:41:16 +0800

    add more test case, clear mock on beforeEach

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -12,7 +12,7 @@ import { AppModule } from './../src/app.module';
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
@@ -21,11 +21,16 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   afterAll(async () => {
     await app.close();
   });
 
-  it('/graphql (GET)', async () => {
+  it('QUERY: posts', async () => {
+    mockPrismaClient.post.findMany.mockResolvedValueOnce([]);
     const response = await request(app.getHttpServer()).post('/graphql').send({
       query: `query {
           posts {
@@ -35,9 +40,29 @@ describe('AppController (e2e)', () => {
           }
         }`,
     });
-    expect(response.body.data.posts).toEqual(
-      await mockPrismaClient.post.findMany(),
-    );
-    expect(mockPrismaClient.post.findMany).toBeCalled();
+    expect(response.body.data.posts).toEqual([]);
+    expect(mockPrismaClient.post.findMany).toBeCalledTimes(1);
+  });
+
+  it('QUERY: postsWhereTitleContains', async () => {
+    mockPrismaClient.post.findMany.mockResolvedValueOnce([]);
+    const response = await request(app.getHttpServer()).post('/graphql').send({
+      query: `query PostWhereTitleContains {
+          posts(where: { title: { contains: "something" } } ) {
+            id
+            title
+            authorId
+          }
+        }`,
+    });
+    expect(response.body.data.posts).toEqual([]);
+    expect(mockPrismaClient.post.findMany).toBeCalledTimes(1);
+    expect(mockPrismaClient.post.findMany).toBeCalledWith({
+      where: {
+        title: {
+          contains: 'something',
+        },
+      },
+    });
   });
 });
