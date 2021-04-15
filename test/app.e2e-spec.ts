f04196be00d3581791ab0b9982d2cad4dcfa9409
jasonlimantoro <jason@ewburgers.com>
2021-04-15 23:17:43 +0800

    setup test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { PostCreateInput } from '../prisma/generated/type-graphql';
+import { prismaClient as prisma } from '../prisma/client';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
@@ -13,12 +15,56 @@ describe('AppController (e2e)', () => {
 
     app = moduleFixture.createNestApplication();
     await app.init();
+    await prisma.post.deleteMany({});
+    await prisma.user.deleteMany({});
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  afterAll(async () => {
+    await app.close();
+    await prisma.$disconnect();
+  });
+
+  it('/graphql (GET)', async () => {
+    const user = await prisma.user.create({
+      data: {
+        email: 'johndoe@example.com',
+        name: 'John',
+      },
+    });
+    const posts: PostCreateInput[] = [
+      {
+        title: 'Some Title',
+        content: 'Some content',
+        author: {
+          connect: {
+            email: user.email,
+          },
+        },
+      },
+      {
+        title: 'Another Title',
+        content: 'Some Other content',
+        author: {
+          connect: {
+            email: user.email,
+          },
+        },
+      },
+    ];
+    await Promise.all(
+      posts.map((post) => {
+        return prisma.post.create({ data: post });
+      }),
+    );
+    const response = await request(app.getHttpServer()).post('/graphql').send({
+      query: `query {
+          posts {
+            id
+            title
+            authorId
+          }
+        }`,
+    });
+    expect(response.body.data.posts).toHaveLength(2);
   });
 });
